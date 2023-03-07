6ce9001b096af0fe8478fa66c5a91f20f690820c
Aditya Shukla <adityashukzy@gmail.com>
2023-03-07 16:54:41 +0530

    Minor fix

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def main():
             st.markdown("1. For general-purpose texts, use bart-large-cnn.\n2. For academic or scientific texts, use bart-easydocs.\n3. The summary produced may not accurately cover all relevant parts of a text. Use this tool only as a starting guide.")
 
         st.subheader("Enter text to summarize")
-        text = st.text_area(height=150, label_visibility="collapsed")
+        text = st.text_area(label="", height=150, label_visibility="collapsed")
 
         min_len_col, max_len_col, model_col = st.columns(3)
 
