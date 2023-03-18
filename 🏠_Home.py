633fe209343bb6da97b74d78311eb95ff86c9720
Aditya Shukla <adityashukzy@gmail.com>
2023-03-18 09:39:17 +0530

    Minor fix

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -14,6 +14,9 @@ st.set_page_config(
 
 # MAIN Function
 def main():
+    with st.sidebar:
+        st.markdown("---")
+
     add_logo("https://ibb.co/S39BWpt")
 
     st.subheader("EasyDocs is a one-stop solution combining all your most needed tools as a student. We understand the value and preciousness of time and that is why we have aimed to make EasyDocs as no-nonsense as possible!")
@@ -21,7 +24,7 @@ def main():
     st.write("👉 Summarize webpages and long text documents and breeze through the essentials!")
     st.write("👉 Scan images for text and have them transcribed. This is called Optical Character Recognition!")
     st.write("👉 Play around with and explore the extremely convenient method of learning: *audiobooks*!")
-    st.write("👉 If you're just looking around, maybe visit EzPz our chatbot and learn about EasyDocs!")
+    # st.write("👉 If you're just looking around, maybe visit EzPz our chatbot and learn about EasyDocs!")
 
 
 if __name__ == "__main__":
